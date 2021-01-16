e15a989e20247f5d43ba4698646e5df9e80e803c
nemial <nemtre@protonmail.com>
2021-01-16 17:46:02 +0200

    Configure database

--- config/database.php
+++ config/database.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Str;
 
 $DATABASE_URL = parse_url(getenv("DATABASE_URL"));
 $host = $DATABASE_URL["host"] ?? '127.0.0.1';
-$port = $DATABASE_URL["port"] ?? '3306';
+$port = $DATABASE_URL["port"] ?? '5432';
 $database = $DATABASE_URL["path"] ? ltrim($DATABASE_URL["path"], "/") : 'forge';
 $username = $DATABASE_URL["user"] ?? 'forge';
 $password = $DATABASE_URL["pass"] ?? '';
@@ -22,7 +22,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -75,11 +75,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'port' => $port,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
