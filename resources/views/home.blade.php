b6d59af4ab540ad29352f0cb765137217dbf8c07
nemial <nemtre@protonmail.com>
2021-04-06 19:04:52 +0200

    Fix translate

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="jumbotron">
-            <h1 class="display-4">{{ __('home.name') }}</h1>
+            <h1 class="display-4">Привет от Хекслета!</h1>
             <p class="lead">@lang('home.description')</p>
             <hr class="my-4">
             <p>@lang('home.project')</p>
