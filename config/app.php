dfd5d8c1dbae5e2a7059c2ea329cfee59d1fe5f3
nemial <nemtre@protonmail.com>
2021-04-06 18:32:39 +0200

    Fix welcome names. Change app url

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'http://obscure-basin-14138.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
--- resources/lang/ru/home.php
+++ resources/lang/ru/home.php
@@ -6,7 +6,7 @@ return [
     'description' => 'Простая реализация менеджера задач',
     'project' => 'Учебный проект',
     'button' => 'Узнать больше',
-    'status' => 'tasksStatuses',
-    'labels' => 'labels',
-    'task' => 'task'
+    'status' => 'Статусы',
+    'labels' => 'Метки',
+    'task' => 'Задачи'
 ];
